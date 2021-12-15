c0c55cd8e413165ba1a8d65b8df4b268af52fdd0
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-12-15 15:01:57 -0300

    iniciando projeto

---
+++ back-end SpringBoot(Java)/src/main/java/com/devoliga/MagicImpostor/MagicImpostorApplication.java
@@ -0,0 +1,13 @@
+package com.devoliga.MagicImpostor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicImpostorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicImpostorApplication.class, args);
+	}
+
+}
---
+++ back-end SpringBoot(Java)/src/test/java/com/devoliga/MagicImpostor/MagicImpostorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devoliga.MagicImpostor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicImpostorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
